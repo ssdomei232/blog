c65fb9718ea806e05c57328f6ae3fa830591d54f
mei <i@mmeiblog.cn>
2024-12-27 14:25:16 +0800

    upgrade fluid && add snowflake

--- scripts/miao.js
+++ scripts/miao.js
@@ -1,4 +1,5 @@
-hexo.extend.injector.register('head_begin', '<script defer src="https://umami.mmeiblog.cn/script.js" data-website-id="f41c8851-4c0f-49e4-9dc4-0d1f227dbba5"></script>', 'default');
+hexo.extend.injector.register('body_end', '<script src="/js/snowflake.js"></script>', 'default');
+// hexo.extend.injector.register('body_end', '<script src="/js/china-lantern.js"></script>', 'default');
 // 预先连接的域名
-hexo.extend.injector.register('head_begin', '<link rel="preconnect" href="https://api.mmeiblog.cn"><link rel="preconnect" href="https://img.mmeiblog.cn"><link rel="preconnect" href="https://img.su-mei.cn"><link rel="preconnect" href="https://lib.baomitu.com">', 'default');
+hexo.extend.injector.register('head_begin', '<link rel="preconnect" href="https://api.mmeiblog.cn"><link rel="preconnect" href="https://img.mmeiblog.cn"><link rel="preconnect" href="https://lib.baomitu.com">', 'default');
 hexo.extend.injector.register('head_begin', '<link rel="preconnect" href="https://q1.qlogo.cn">', 'about');
